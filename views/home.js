996d94df14cdad18b6978f6bf0a0b73d14cf3325
InaMiron <fsuciue@gmail.com>
2018-07-06 00:15:51 +0300

    new

--- views/home.js
+++ views/home.js
@@ -3,66 +3,66 @@ window.onload=Onloaded;
 
 
 function Onloaded(){
-	// const viewData=new Movies();
-	// viewData.getMovies()
-	// .then(createMovieList)
-	// .catch(CreateMovieListError);
+	const viewData=new Movies();
+	viewData.getMovies()
+	.then(createMovieList)
+	.catch(CreateMovieListError);
 
 
 
-	// function createMovieList(){
-	// 	console.log(viewData.itemList);
-	// 	const containElements=document.getElementById("container");
+	function createMovieList(){
+		console.log(viewData.itemList);
+		const containElements=document.getElementById("movieListContainer");
 		
-	// 		for(let i=0;i<viewData.itemList.length;i++){
-	// 			const divElements=document.createElement('div');
-	// 			divElements.classList.add('item');
-	// 			containElements.appendChild(divElements);
-	// 			containElements.firstChild.classList.add('active');
+			for(let i=0;i<viewData.itemList.length;i++){
+				const divElements=document.createElement('div');
+				divElements.classList.add('item');
+				containElements.appendChild(divElements);
+				//containElements.firstChild.classList.add('active');
 
-	// 			const item=viewData.itemList[i];
+				const item=viewData.itemList[i];
 
 
-	// 			const title=document.createElement("a");
-	// 			title.setAttribute('href', basePath + '/pages/movieDetails.html?movieId='+item._id);
-	// 			title.setAttribute('target','blank');
-	// 			title.innerHTML=item.Title+'<br>';
+				const title=document.createElement("a");
+				//title.setAttribute('href', basePath + '/pages/movieDetails.html?movieId='+item._id);
+				title.setAttribute('target','blank');
+				title.innerHTML=item.Title+'<br>';
 
-	// 			const picture=document.createElement('img');
-	// 			picture.setAttribute('src',item.Poster);
-	// 			picture.setAttribute('alt','404');
-	// 			picture.setAttribute('width','200px');
-	// 			picture.classList.add('img-size');
+				const picture=document.createElement('img');
+				picture.setAttribute('src',item.Poster);
+				picture.setAttribute('alt','404');
+				picture.setAttribute('width','200px');
+				picture.classList.add('img-size');
 
-	// 			const genre=document.createElement('p');
-	// 			genre.innerHTML=item.Genre;
+				const genre=document.createElement('p');
+				genre.innerHTML=item.Genre;
 
-	// 			const runtime=document.createElement('p');
-	// 			runtime.innerHTML=item.Runtime;
+				const runtime=document.createElement('p');
+				runtime.innerHTML=item.Runtime;
 
-	// 			const type=document.createElement('p');
-	// 			type.innerHTML=item.Type;
+				const type=document.createElement('p');
+				type.innerHTML=item.Type;
 
-	// 			const year=document.createElement('p');
-	// 			year.innerHTML=item.Year;
+				const year=document.createElement('p');
+				year.innerHTML=item.Year;
 
-	// 			const country=document.createElement('p');
-	// 			country.innerHTML=item.Country;
+				const country=document.createElement('p');
+				country.innerHTML=item.Country;
 
-	// 			const button=document.createElement('button');
-	// 			button.setAttribute('data-id',item._id);
-	// 			button.innerText="Delete";
+				const button=document.createElement('button');
+				button.setAttribute('data-id',item._id);
+				button.innerText="Delete";
 
-	// 			divElements.appendChild(picture);
-	// 			divElements.appendChild(title);
-	// 			divElements.appendChild(country);
-	// 			divElements.appendChild(genre);
-	// 			divElements.appendChild(type);
-	// 			divElements.appendChild(year);
-	// 			divElements.appendChild(runtime);
-	// 			divElements.appendChild(button);
-	// 		}
-	// 	}
+				divElements.appendChild(picture);
+				divElements.appendChild(title);
+				divElements.appendChild(country);
+				divElements.appendChild(genre);
+				divElements.appendChild(type);
+				divElements.appendChild(year);
+				divElements.appendChild(runtime);
+				divElements.appendChild(button);
+			}
+		}
 		//Log In functionality
 		//Submit button
 		const loginButton = document.querySelector("[name='login']");
